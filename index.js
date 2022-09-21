0425c0339c0262419057d1ff761e002f1183be23
rowjay007 <rowlives@gmail.com>
2022-09-21 11:25:57 +0100

    fetch transfer news

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
+// @ts-nocheck
 const express = require("express");
 const cheerio = require("cheerio");
 const axios = require("axios");
 
 const app = express();
 
+const details = [];
 const PORT = 8080;
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json("Hello World!");
+});
+
+app.get("/transfer", (req, res) => {
+  axios
+    .get("https://www.dailymail.co.uk/sport/transfernews")
+    .then(function (response) {
+      const html = response.data;
+      const $ = cheerio.load(html);
+      $('a:contains("transfer")', html).each(function () {
+        const title = $(this).text();
+        const url = $(this).attr("href");
+        details.push({
+          title,
+          url,
+        });
+      });
+      res.json(details);
+    })
+    .catch((err) => console.log(err));
 });
 
 app.listen(PORT, () => {
